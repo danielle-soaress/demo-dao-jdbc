b1d865227ca6a11262ff780eaa5f6c06a03bd48b
Danielle Soares <silvasoaresdanielle2@gmail.com>
2024-08-12 23:39:58 -0300

    Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department ("Books", 1);
-		System.out.println(obj);
-
+		Seller seller = new Seller (2, "Bob", "bob@gmail.com", new Date(), 30000.0, obj);
+		System.out.println(seller);
 	}
 
 }
